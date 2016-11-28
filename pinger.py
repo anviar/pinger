abcd02ac15a06828e94ddda6c53970dfca43f5a4
oleg <oleg.kluchkin@gmail.com>
2016-11-28 20:52:15 +0300

    Initial commit

---
+++ pinger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import socks
+import argparse
+import sys
+import time
+
+# Preparing arguments
+argparser=argparse.ArgumentParser(description='Check service health')
+argparser.add_argument('--host', help='hostname', type=str, required=True)
+argparser.add_argument('--port', help='port', type=int, required=True)
+argparser.add_argument('--protocol', help='protocol version', type=int, required=True)
+argparser.add_argument('--key', help='client key', type=str, required=True)
+argparser.add_argument('--envid', help='environment id', type=int, required=True)
+args = argparser.parse_args()
+
+# loading requested protocol
+protocol = __import__('protocol.v' + str(args.protocol), globals(), locals(), ['object'], -1)
+
+session = socks.socksocket()
+session.set_proxy(socks.SOCKS4, args.host, 443)
+session.connect(('127.0.0.1', args.port))
+
+protocol.opcode_init(session, args.protocol,args.envid, args.key)
+for i in range (0,5):
+    protocol.opcode_ping(session)
+    time.sleep(0.3)
+session.close()
