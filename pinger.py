fe6d33eec0e7245f3db1fa887475b6192dd485ae
Oleg Kluchkin <oleg.kluchkin+github@gmail.com>
2016-12-20 14:50:48 +0300

    Migrate to python3 Begin login implementation

--- pinger.py
+++ pinger.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import socks
 import argparse
-import time,datetime
+import time,datetime,importlib
 
 # Preparing arguments
 argparser=argparse.ArgumentParser(description='Check service health')
@@ -11,24 +11,27 @@ argparser.add_argument('--port', help='port', type=int, required=True)
 argparser.add_argument('--protocol', help='protocol version', type=int, required=True)
 argparser.add_argument('--key', help='client key', type=str, required=True)
 argparser.add_argument('--envid', help='environment id', type=int, required=True)
+#argparser.add_argument('--user', help='username', type=str, required=True)
+#argparser.add_argument('--password', help='password', type=str, required=True)
 args = argparser.parse_args()
-print args
+print ( args )
 
 # loading requested protocol
-protocol = __import__('protocol.v' + str(args.protocol), globals(), locals(), ['object'], -1)
+protocol = importlib.import_module( 'protocol.v' + str(args.protocol))
 
 session = socks.socksocket()
 session.set_proxy(socks.SOCKS4, args.host, 443)
 session.connect(('127.0.0.1', args.port))
 
 protocol.opcode_init(session, args.protocol,args.envid, args.key)
+#protocol.opcode_login(session, args.user, args.password)
 for i in range (0,5):
     start_time = datetime.datetime.now()
     protocol.opcode_ping(session)
     end_time = datetime.datetime.now()
     response_delay = (end_time - start_time).microseconds / 1000
     if response_delay > 500:
-        print "Response time too long:", response_delay
+        print ( "Response time too long:", response_delay )
         exit(1)
     time.sleep(0.3)
 session.close()
--- protocol/v5.py
+++ protocol/v5.py
@@ -1,25 +1,39 @@
 import struct
 
 def bytes_to_str(s):
-    return "-".join("{:02x}".format(ord(c)) for c in s)
+    return "-".join("{:02x}".format(ord(chr(c))) for c in s)
 
 def opcode_init(session, protocol, envid, key):
     data = struct.pack('<HLHBL' + str(len(key)) + 's',
-                        1, 47, protocol, envid, len(key), key)
-    print 'init>', bytes_to_str( data )
+                        1, 47, protocol, envid, len(key), bytes(key,'UTF-8'))
+    print ( 'init>', bytes_to_str( data ) )
     session.send(data)
     data=session.recv(16)
-    print 'init<', bytes_to_str(data)
+    print ( 'init<', bytes_to_str(data) )
     answer=struct.unpack('<HIBBB', data)
     if answer[0] != 1 or answer[3] != protocol:
-        print "Wrong response"
+        print ( "Wrong response" )
         exit(1)
 
 def opcode_ping(session):
     data = struct.pack('<HL', 0,0)
-    print 'ping>', bytes_to_str(data)
+    print ( 'ping>', bytes_to_str(data) )
     session.send(data)
     data = session.recv(16)
-    print 'pong<', bytes_to_str(data)
+    print ( 'pong<', bytes_to_str(data) )
     data_size = struct.unpack('<HL', data[:6])[1]
     struct.unpack('<HL' + str(data_size) + 'b', data)
+
+def opcode_login(session, username, password):
+    print('login>', end=" ")
+    data = struct.pack('<HI' + str(len(username)) + 'sI' + str(len(password)) + 's',
+                        3,
+                        len(username), bytes(username, 'UTF-8'),
+                        len(password), bytes(password, 'UTF-8'))
+    print ( bytes_to_str(data) )
+    session.send(data)
+    print('login<', end=" ")
+    data = session.recv(1)
+    print ( bytes_to_str(data) )
+    #data_size = struct.unpack('<HL', data[:6])[1]
+    #truct.unpack('<HL' + str(data_size) + 'b', data)
