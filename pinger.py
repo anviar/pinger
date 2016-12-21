186128ebddcf476d93a4972254348f7bb70e267f
Oleg Kluchkin <oleg.kluchkin+github@gmail.com>
2016-12-21 13:58:57 +0300

    Added optional arguments

--- pinger.py
+++ pinger.py
@@ -9,6 +9,8 @@ argparser=argparse.ArgumentParser(description='Check service health')
 argparser.add_argument('--host', help='hostname', type=str, required=True)
 argparser.add_argument('--port', help='port', type=int, required=True)
 argparser.add_argument('--protocol', help='protocol version', type=int, required=True)
+argparser.add_argument('--ping', help='number of ping iterations, default 5', type=int, default=5)
+argparser.add_argument('--timeout', help='timeout for socket operations, default 5', type=float, default=5)
 argparser.add_argument('--key', help='client key', type=str, required=True)
 argparser.add_argument('--envid', help='environment id', type=int, required=True)
 argparser.add_argument('--user', help='username', type=str )
@@ -21,12 +23,12 @@ protocol = importlib.import_module( 'protocol.v' + str(args.protocol))
 
 session = socks.socksocket()
 session.set_proxy(socks.SOCKS4, args.host, 443)
-session.settimeout(5)
+session.settimeout(args.timeout)
 session.connect(('127.0.0.1', args.port))
 protocol.opcode_init(session, args.protocol,args.envid, args.key)
 if args.user is not None and args.password is not None:
     protocol.opcode_login(session, args.user, args.password)
-for i in range (0,5):
+for i in range (0, args.ping):
     start_time = datetime.datetime.now()
     protocol.opcode_ping(session)
     end_time = datetime.datetime.now()
