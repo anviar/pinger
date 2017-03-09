4a20c5378cb64ec632a3b9f6fc755095d04020c8
Oleg Kluchkin <oleg.kluchkin+github@gmail.com>
2017-03-09 16:39:39 +0300

    Added ping timeout option

--- pinger.py
+++ pinger.py
@@ -11,6 +11,7 @@ argparser.add_argument('--port', help='port', type=int, required=True)
 argparser.add_argument('--protocol', help='protocol version', type=int, required=True)
 argparser.add_argument('--ping', help='number of ping iterations, default 5', type=int, default=5)
 argparser.add_argument('--timeout', help='timeout for socket operations, default 5', type=float, default=5)
+argparser.add_argument('--ping-timeout', help='timeout for socket operations, default 5', type=int, default=2000)
 argparser.add_argument('--key', help='client key', type=str, required=True)
 argparser.add_argument('--envid', help='environment id', type=int, required=True)
 argparser.add_argument('--user', help='username', type=str )
@@ -33,7 +34,7 @@ for i in range (0, args.ping):
     protocol.opcode_ping(session)
     end_time = datetime.datetime.now()
     response_delay = (end_time - start_time).microseconds / 1000
-    if response_delay > 500:
+    if response_delay > args.ping_timeout:
         print ( "Response time too long:", response_delay )
         exit(1)
     time.sleep(0.3)
